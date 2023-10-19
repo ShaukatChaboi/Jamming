ed0a0833da85cb3f5a9d9436294256067d97fe4a
aragulshan <aragulshan321@gmail.com>
2023-10-20 02:51:51 +0500

    feat: added track list, track, conn files

---
+++ src/components/playlist/PlayLists.js
@@ -0,0 +1,44 @@
+// import React from "react";
+import React, { useState, useCallback } from "react";
+import TrackList from "../TrackList/TrackLists";
+
+const Playlist = (props) => {
+  const [searchValue, setSearchValue] = useState([]);
+  const handleNameChange = (event) => {
+    const searchedValue = event.target.value;
+    setSearchValue(searchedValue);
+    console.log(searchedValue, 'valueeee');
+  };
+  //   const handleNameChange = useCallback(
+  //     (event) => {
+  //       props.onNameChange(event.target.value);
+  //     },
+  //     [props.onNameChange]
+  //   );
+
+  return (
+    <div className="flex flex-col items-center  w-[37%] rounded-[5px] max-h-[950px] py-[2.27rem] px-[1.16rem] bg-[#010c3fb3] shadow-md">
+      {/* <div className="flex flex-col items-center overflow-y-scroll w-2/5 max-h-96 p-6 bg-opacity-70 bg-blue-900 shadow-md"> */}
+      <input
+        // value={searchValue}
+        onChange={handleNameChange}
+        defaultValue={"New Playlist"}
+        className="w-full border-0 outline-[0] bg-[transparent] border-b border-[#6f6f6f] text-[#fff] text-[1.55rem]"
+        // className="w-full border-0 outline-none bg-transparent border-b border-gray-400 text-white text-xl"
+      />
+      <TrackList
+        tracks={props.playlistTracks}
+        isRemoval={true}
+        onRemove={props.onRemove}
+      />
+      <button
+        className="cursor-pointer w-[10rem] py-[0.77rem] px-[0] rounded-full border-0 mt-5 bg-[#6c41ec] text-center text-[0.83rem] transition duration-250 bg-white font-semibold hover:bg-[#6c41e9b3] text-white"
+        onClick={props.onSave}
+      >
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+};
+
+export default Playlist;
---
+++ src/conn/Spotify.js
@@ -0,0 +1,9 @@
+const clientId = "";
+const redirectUri = "http://localhost:3000/";
+let accessToken;
+
+const Spotify = () => {
+  return <div></div>;
+};
+
+export default Spotify;
