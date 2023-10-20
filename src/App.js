46eaf8c664c23a91def2f29ca096bb2f863105ad
aragulshan <aragulshan321@gmail.com>
2023-10-20 14:19:11 +0500

    feat: added spotify-conn and SearchResults component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
 import React from "react";
-// import Routers from "./routers";
-import Navbar from "./components/navbar/index";
-import Playlist from "./components/playlist/PlayLists";
-import SearchInput from "./components/SearchInput/SearchInput";
-import Spotify from "./conn/Spotify";
+import Routers from "./routers";
 
 function App() {
-  // const search = useCallback((term) => {
-  //   Spotify.search(term).then(setSearchResults);
-  // }, []);
   return (
     <>
-      <Navbar />
-      <div className="App">
-        <SearchInput />
-        <div className="flex flex-row justify-between mx-auto py-12">
-          <Playlist />
-        </div>
-        {/* <Routers /> */}
-      </div>
+      <Routers />
     </>
   );
 }
