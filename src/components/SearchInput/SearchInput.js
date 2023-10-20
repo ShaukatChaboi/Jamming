34096637261df402115712fac109b8367737d77e
ShaukatChaboi <141963928+ShaukatChaboi@users.noreply.github.com>
2023-10-21 01:10:27 +0500

    .search logic  added

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,26 +1,46 @@
 import React, { useState, useCallback } from "react";
-function SearchInput(props) {
-  const [searchTerm, setSearchTerm] = useState("");
+ function SearchInput(props) {
+//   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleTermChange = useCallback((event) => {
-    setSearchTerm(event.target.value);
-  }, []);
-  const handleSearch = useCallback(() => {
+// const handleTermChange = useCallback((event) => {
+//   setSearchTerm(event.target.value);
+// }, []);
+
+// const handleSearch = useCallback(() => {
+//   setSearchTerm((currentSearchTerm) => {
+//     props.onSearch(currentSearchTerm);
+//     return currentSearchTerm; // Return the current value to set it immediately
+//   });
+// }, [props.onSearch]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const search = () => {
     props.onSearch(searchTerm);
-  }, [props.onSearch, searchTerm]);
+  };
+
+  const handleTermChange = useCallback((e) => {
+    setSearchTerm(e.target.value);
+  }, []);
+
+  const handleKeyUp = useCallback((e) => {
+    if (e.keyCode === 13) search();
+  }, [search]);
+  
   return (
     <>
       <div className="flex items-center flex-col mb-100  pt-40 mb-8">
         <input
+          onChange={handleTermChange}
+          onKeyUp={handleKeyUp}
           type="text"
           placeholder="Enter A Song, Album, or Artist .... "
-          className="block flex-1 border-0 decoration-black bg-Jamming-textColor pl-1 text-Jamming-bgTextInput   border-white text-lg my-6 py-2 px-0 text-left w-72"
-          onChange={handleTermChange}
+          className="block flex-1 border-0 decoration-black bg-Jamming-textColor pl-1 text-Jamming-bgTextInput rounded-lg border-white text-lg my-6 py-2 px-0 text-left w-72"
+          
         />
         <button
           className="bg-Jamming-btn  border-0 text-center w-28
       py-2 px-0 font-medium text-btnfontSize cursor-pointer rounded-full text-white hover:bg-Jamming-btnHover "
-          onClick={handleSearch}
+          onClick={search}
         >
           SEARCH
         </button>
--- src/components/SearchResults/searchresult.js
+++ src/components/SearchResults/searchresult.js
@@ -4,8 +4,8 @@ import TrackList from "../TrackList/TrackLists";
 const SearchResult = (props) => {
   return (
     <div className=" w-[50%] rounded-[5px] h-[950px] p-[0.88rem] bg-[#010c3fb3] shadow-Jamming-950 bg-opacity-70 ">
-      <h2>Results</h2>
-      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+      <h2 className="text-2xl pe-[70%]">Results</h2>
+      <TrackList  onAdd={props.onAdd} isRemoval={false} tracks={props.searchResults} />
     </div>
   );
 };
